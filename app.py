5038b9538606154154ff13f88d903246469bfe26
ErikaVychlopenova <pvpsammi@gmail.com>
2023-05-02 21:04:17 +0100

    ADD: saving data to file

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import serial
 async_mode = None
 import MySQLdb 
 import configparser as ConfigParser
-async_mode = None
+# async_mode = None
 
 app = Flask(__name__)
 
@@ -21,7 +21,7 @@ app = Flask(__name__)
 # print(myhost)
 
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode=async_mode)
+socketio = SocketIO(app)
 thread = None
 thread_lock = Lock() 
 
@@ -29,37 +29,45 @@ ser = serial.Serial("/dev/ttyS0")
 ser.baudrate = 9600
 ser.flushInput()
 
+shouldMonitor = False       
+
 def background_thread(args):
     count = 0    
     dataCounter = 0
     # db = MySQLdb.connect(host=myhost,user=myuser,passwd=mypasswd,db=mydb)
     dataList = []
+    dataToSave = []
+    state = False;
+    global shouldMonitor
     i = 0
     while True:
         if args:
-          A = dict(args).get('A')
-          btnV = dict(args).get('btn_value')
+            A = dict(args).get('A')
+            btnV = dict(args).get('btn_value')
+            monitorBtn = dict(args).get('monitorBtnVal')
         else:
-          A = 1
-          btnV = 'null' 
-        
+            A = 1
+            btnV = 'null' 
+            monitorBtn = False
+          
         socketio.sleep(2)
         count +=1
+        print(args)
         ###############SENSOR DATA#####################
-        #line = ser.readline().decode('utf-8').rstrip()
-        #data = json.loads(line)
+        line = ser.readline().decode('utf-8').rstrip()
+        data = json.loads(line)
         ###############SENSOR DATA#####################
         
         ###############TEST DATA######################
-        f = open('dummydata.json')
-        line = json.load(f)
-        data = line[i]
+        #f = open('dummydata.json')
+        #line = json.load(f)
+        #data = line[i]
         ###############TEST DATA######################
+        
         temp = data['temperature']
         hum = data['humidity']
         dist = data['distance']
         ir = data['ir']
-        
         dataDict = {
              "time": count,
              "t": temp,
@@ -67,42 +75,76 @@ def background_thread(args):
              "d": dist,
              "ir": ir}
         dataList.append(dataDict)
+        print(shouldMonitor)
+        print(state)
+        if shouldMonitor or ir == 0:
+            print("zapisujem")
+            dataToSave.append(dataDict)
+            state = True
+        if state and not shouldMonitor:
+            print("ukladam do suboru")
+            state = False
+            i = i+1
+            f = open("data.txt", "a")
+            f.write("id: "+ str(i) +" - "+ json.dumps(dataToSave)+"\n")
+            f.close()
+            dataToSave = []
+			
+			
+			
         
-        i = i+1
         if len(dataList)>0:
             print(str(dataList))
             print(str(dataList).replace("'", "\""))
+             
         socketio.emit('my_response',
-                      {'data': dataDict, 'count': count},
-                      namespace='/test')
-
+                      {'data': dataDict, 'count': count})
+        
+        # if(saveDataNow == True): 
+            # with open("data.json", "w") as f:
+                # json.dump(dataList, f)
+           
+def search_and_parse(index):
+	f = open("data.txt", "r")
+	for line in f:
+		id_str, json_str = line.strip().split(' - ',1)
+		id_int = int(id_str.split(':')[1].strip())
+		print(id_int)
+		print(index)
+		if id_int == index:
+			data = json.loads(json_str)
+			return data
+	return []                      
+                      
+                     
 @app.route('/')
 def index():
-    return render_template('index.html', async_mode=socketio.async_mode)
+    return render_template('index.html')
     
-@app.route('/graphlive', methods=['GET', 'POST'])
-def graphlive():
-    return render_template('graphlive.html', async_mode=socketio.async_mode)
+# @app.route('/graphlive', methods=['GET', 'POST'])
+# def graphlive():
+    # return render_template('graphlive.html', async_mode=socketio.async_mode)
   
-@app.route('/gauge', methods=['GET', 'POST'])
-def gauge():
-    return render_template('gauge.html', async_mode=socketio.async_mode)
+# @app.route('/gauge', methods=['GET', 'POST'])
+# def gauge():
+    # return render_template('gauge.html', async_mode=socketio.async_mode)
 
-@socketio.on('my_event', namespace='/test')
-def test_message(message):   
-    session['receive_count'] = session.get('receive_count', 0) + 1 
-    session['A'] = message['value']    
-    emit('my_response',
-         {'data': message['value'], 'count': session['receive_count']})
+@socketio.on('my_event')
+def test_message(message):  
+	if(message['value'] == ""): 
+		return
+	response = search_and_parse(int(message['value']))
+	emit('file_response',
+         {'data': response})
  
-@socketio.on('disconnect_request', namespace='/test')
+@socketio.on('disconnect_request')
 def disconnect_request():
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
          {'data': 'Disconnected!', 'count': session['receive_count']})
     disconnect()
 
-@socketio.on('connect', namespace='/test')
+@socketio.on('connect')
 def test_connect():
     global thread
     with thread_lock:
@@ -110,11 +152,24 @@ def test_connect():
             thread = socketio.start_background_task(target=background_thread, args=session._get_current_object())
     emit('my_response', {'data': 'Connected', 'count': 0})
 
-@socketio.on('click_event', namespace='/test')
+@socketio.on('click_event')
 def db_message(message):   
-    session['btn_value'] = message['value']    
+    session['btn_value'] = message['value']  
+    
+    
+@socketio.on('monitoringButton')
+def handle_message(data):
+    global shouldMonitor
+    if data['status'] == 'true':
+        print('Monitoring button is on')
+        shouldMonitor = True
+        print(shouldMonitor)
+    elif data['status'] == 'false':
+        print('Monitoring button is off')
+        shouldMonitor = False
+	
 
-@socketio.on('disconnect', namespace='/test')
+@socketio.on('disconnect')
 def test_disconnect():
     print('Client disconnected', request.sid)
 
